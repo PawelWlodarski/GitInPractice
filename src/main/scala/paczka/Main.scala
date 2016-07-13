d2159e74e60b77c4d9aa0ba0babac1542098d906
pawel <pawel@domena.pl>
2016-07-13 23:26:58 +0200

    basic calc

--- src/main/scala/paczka/Main.scala
+++ src/main/scala/paczka/Main.scala
@@ -3,9 +3,15 @@ package paczka
 object Main{
 
 	def main(args: Array[String]){
-		println("dziala!!!")
+		val result=Calc.add(13,5)
+		println(s"result=$result")
 	}
 
 	class JugMeeting(topic:String,participants:Int)
 
+}
+
+object Calc{
+	def add(i1:Int,i2:Int) = i1+i2
+	def sub(i1:Int,i2:Int) = i1-i2
 }
